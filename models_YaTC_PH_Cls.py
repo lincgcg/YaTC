16692980509004fcbde324b567682b9fc3d9fa3a
“lincgcg <chunganglin@gmail.com>
2023-09-25 14:05:01 +0800

    debug

--- models_YaTC_PH_Cls.py
+++ models_YaTC_PH_Cls.py
@@ -100,8 +100,6 @@ class TrafficTransformer(timm.models.vision_transformer.VisionTransformer):
         return outcome
 
     def forward_head(self, x, pre_logits: bool = False):
-        if self.global_pool:
-            x = x[:, self.num_prefix_tokens:].mean(dim=1) if self.global_pool == 'avg' else x[:, 0]
         x = self.fc_norm(x)
         x = self.head_drop(x)
         return x if pre_logits else self.head(x)
@@ -115,7 +113,7 @@ class TrafficTransformer(timm.models.vision_transformer.VisionTransformer):
 
     def forward(self, x):
         x = self.forward_features(x)
-        # x = self.forward_PH(x)
+        x = self.forward_PH(x)
         x = self.forward_head(x)
         return x
 
